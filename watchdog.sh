e2e293ca07846d492cde5891c48bdfdb1e182583
daltonhahn <hahnd11@gmail.com>
2021-04-30 10:49:17 -0500

    Working on watchdog report script

--- watchdog.sh
+++ watchdog.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-aws ec2 describe-instances --query 'Reservations[].Instances[][PublicIpAddress]' --filter "Name=instance-state-name,Values=running" "Name=tag:Watchdog,Values=Yes" --output text
+IPS=$(aws ec2 describe-instances --query 'Reservations[].Instances[][PublicIpAddress]' --filter "Name=instance-state-name,Values=running" "Name=tag:Watchdog,Values=Yes" --output text)
+for i in $IPS
+do
+	echo $i
+	# curl their missing file
+	# increment file num
+	# diff all files together?
+done
